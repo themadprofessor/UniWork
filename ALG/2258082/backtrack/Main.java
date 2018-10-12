339a4ee16006af5c5115c5337edc2a4ee99eabe3
Stuart Reilly <stu@reilly-family.co.uk>
2018-10-12 10:17:21 +0100

    Add ALG exe

---
+++ ALG/2258082/backtrack/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ program to find shortest path using the backtrack search algorithm
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		long start = System.currentTimeMillis();
+
+		String inputFileName = args[0]; // input file name
+  
+		FileReader reader = new FileReader(inputFileName);
+		Scanner in = new Scanner(reader);
+		
+		// read in the data here and create graph here
+		
+		reader.close();
+
+		// do the work here
+
+		// end timer and print total time
+		long end = System.currentTimeMillis();
+		System.out.println("\nElapsed time: " + (end - start) + " milliseconds");
+	}
+
+}
