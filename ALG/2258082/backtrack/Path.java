f33a2097c397bebcb0f4d09aa8ca26f43b877251
Stuart Reilly <stu@reilly-family.co.uk>
2018-11-05 16:41:16 +0000

    Impl Backtrack

--- ALG/2258082/backtrack/Path.java
+++ ALG/2258082/backtrack/Path.java
@@ -1,4 +1,6 @@
 import java.util.ArrayDeque;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Path {
     private ArrayDeque<Integer> path;
@@ -10,6 +12,15 @@ public class Path {
         weight = 0;
     }
 
+    public static Path max_path() {
+        return new Path(new ArrayDeque<>(), Integer.MAX_VALUE);
+    }
+
+    private Path(ArrayDeque<Integer> path, int weight) {
+        this.path = path;
+        this.weight = weight;
+    }
+
     public void pushEdge(Node n) {
         path.push(n.getIndex());
         weight += n.getWeight();
@@ -18,5 +29,24 @@ public class Path {
     public void popEdge(Node n) {
         path.pop();
         weight -= n.getWeight();
+    }
+
+    public int lastVertex() {
+        return path.peek();
+    }
+
+    public String path() {
+        StringBuilder builder = new StringBuilder();
+        while (!path.isEmpty()) {
+            builder.append(path.pollLast());
+            builder.append(" ");
+        }
+
+        return builder.toString();
+    }
+
+    @Override
+    protected Object clone() {
+        return new Path(this.path.clone(), this.weight);
     }
 }
