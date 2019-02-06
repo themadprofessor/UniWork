ea96db8a8bf1f7a076b271cdd92e15e1eb4cd706
Stuart Reilly <stu@reilly-family.co.uk>
2019-02-06 16:37:15 +0000

    Impl dnslookup Add os skeleton

---
+++ NET/dnslookup/dnslookup.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#define BUFFER_SIZE 39 // max size of IPv6 address string
+#define ERR_ARGS 1
+#define ERR_ADDRINFO 2
+#define ERR_CONV 3
+
+int main(int argc, char* argv[]) {
+    if (argc <= 1) {
+        eprintf("must specify domain\n");
+        return ERR_ARGS;
+    }
+
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = PF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    for (int i = 1; i < argc; i++) {
+        struct addrinfo* addrinfo_list;
+        int error;
+
+        if ((error = getaddrinfo(argv[i], "80", &hints, &addrinfo_list)) != 0) {
+            eprintf("unable to lookup address of [%s]: %s", argv[i], gai_strerror(error));
+            return ERR_ADDRINFO;
+        }
+
+        struct addrinfo* curr_addr;
+        for (curr_addr = addrinfo_list; curr_addr != NULL; curr_addr = curr_addr->ai_next) {
+            char buffer[BUFFER_SIZE] = {0};
+            if (inet_ntop(curr_addr->ai_family, &((struct sockaddr_in*)curr_addr->ai_addr)->sin_addr, buffer, sizeof(buffer)) == NULL) {
+                perror("unable to convert IP address to string");
+                return ERR_CONV;
+            }
+            printf("%s %s %s\n", argv[i], curr_addr->ai_family == AF_INET6 ? "IPv6" : "IPv4", buffer);
+        }
+    }
+
+    return 0;
+}
