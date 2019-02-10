0bb369aa7049b0dbd77dd2be967b95b05d08ecb9
stuart <stu@reilly-family.co.uk>
2019-02-10 16:03:00 +0000

    Fix graph_gen.sh

--- NET/dnslookup/graph_gen.sh
+++ NET/dnslookup/graph_gen.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 domains="www.google.com www.reddit.com www.disroot.org stackoverflow.com moodle.gla.ac.uk www.tldp.com www.gnu.org unix.stackexchange.com 4chan.org github.com gitlab.com facebook.com en.wikibooks.org mariadb.com js.masterpassword.app"
 
-{ echo "graph routertopology {" && ./dnslookup $domains | awk '{print $3}' | sed '/:/d' | xargs -d '\n' -n 1 traceroute -q 1 -n | sed '/\*/d' | sed '/to/d' | awk 'NR>1{print "\"" f "\" -- \"" $2 "\""} {f=$2}' | sort | uniq && echo "}"; } > router-4.dot
-{ echo "graph routertopology {" && ./dnslookup $domains | awk '{print $3}' | sed '/\./d' | xargs -d '\n' -n 1 traceroute -q 1 -n | sed '/\*/d' | sed '/to/d' | awk 'NR>1{print "\"" f "\" -- \"" $2 "\""} {f=$2}' | sort | uniq && echo "}"; } > router-6.dot
+parse_trace () {
+	traceroute -q 1 -n $1 | sed '/\*/d' | sed '/to/d' | awk 'NR>1{print "\"" f "\" -- \"" $2 "\""} {f=$2}'
+}
+export -f parse_trace
+
+{ echo "graph routertopology {" && ./dnslookup $domains | awk '{print $3}' | sed '/:/d' | xargs -d '\n' -I "%" -n 1 bash -c 'parse_trace "%"' | sort | uniq && echo "}"; } > router-4.dot
+{ echo "graph routertopology {" && ./dnslookup $domains | awk '{print $3}' | sed '/\./d' | xargs -d '\n' -n 1 -I "%" -n 1 bash -c 'parse_trace "%"'  | sort | uniq && echo "}"; } > router-6.dot
 dot -Tpdf router-4.dot -o router-topology-v4.pdf
 dot -Tpdf router-6.dot -o router-topology-v6.pdf
