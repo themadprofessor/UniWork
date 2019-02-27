33570592862603e30c109f9f482fc4d3456cfefb
stuart <stu@reilly-family.co.uk>
2019-02-27 10:10:16 +0000

    Add tester for os

---
+++ OS/2258082r/test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+makefile=${1:-Makefile}
+threads=("1" "2" "4" "8" "16" "128")
+sizes=("32" "64" "1024" "65536" "100000")
+
+for thread in ${threads[@]}; do
+    for size in ${sizes[@]}; do
+        sed -i "s/-DNTH=[0-9]*/-DNTH=$thread/" ${makefile}
+        sed -i "s/-DBUF_SZ=[0-9]*/-DBUF_SZ=$size/" ${makefile}
+        make -f ${makefile} -B > /dev/null
+        echo -n "Testing $thread threads and $size size: "
+        output=`./testbench`
+        if [[ $(echo ${output} | sed 's/SUCCESS!/SUCCESS!\n/g' | grep -c SUCCESS -) != ${thread} ]]; then
+            echo ${output} | sed 's/SUCCESS!/SUCCESS!\n/g' > "$thread-$size.out"
+            echo "Failed, dumping output to $thread-$size.out"
+        else
+            echo "Success"
+        fi
+    done
+done
