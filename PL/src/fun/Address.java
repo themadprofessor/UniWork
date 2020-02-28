54625dc342de9f27c93ceb9aea8eb8a8baf81f1c
stuart <stu@reilly-family.co.uk>
2020-02-28 14:31:32 +0000

    Add pl work

---
+++ PL/src/fun/Address.java
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////
+//
+// Representation of FunVM code, global, and local addresses.
+//
+// Developed June 2012 by David Watt (University of Glasgow).
+//
+//////////////////////////////////////////////////////////////
+
+package fun;
+
+public class Address {
+
+	public static final int
+			CODE   = 0,
+			GLOBAL = 1,
+			LOCAL  = 2;
+
+	public int offset;
+	public int locale;  // CODE, GLOBAL, or LOCAL
+
+	public Address (int off, int loc) {
+		offset = off;  locale = loc;
+	}
+
+}
